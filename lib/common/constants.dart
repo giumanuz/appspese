e9b84caa56777d198902d00a4bc2da38f1bb5ad0
giumanuz <giuliomanuzzi@gmail.com>
2023-02-17 00:12:14 +0100

    aggiusta il cazzo di add_expense_screen

--- lib/common/constants.dart
+++ lib/common/constants.dart
@@ -21,21 +21,34 @@ List<PersonalCard> esempioCards = [
 ];
 
 List<Payment> esempiPayments = [
-  // Payment(
-  //     from: 'Giuliu', amount: 3000, category: 'Discoteca', data: '01/01/1212', to: {'Giuliu': 1000, 'Cats': 1000, 'Berlino': 1000}),
-  // Payment(from: 'Cats', amount: 4000, category: 'Spesa', data: '02/01/1212', to: {'Giuliu': 1000, 'Cats': 2000, 'Berlino': 1000}),
-  // Payment(
-  //     from: 'Berlino', amount: 5, category: 'Porno', data: '03/01/1212', to: {'Berlino': 2, 'Cats': 3}),
+  Payment(
+      from: 'Giuliu',
+      amount: 3000,
+      category: 'Discoteca',
+      data: '01/01/1212',
+      to: {'Giuliu': 1000, 'Cats': 1000, 'Berlino': 1000}),
+  Payment(
+      from: 'Cats',
+      amount: 4000,
+      category: 'Spesa',
+      data: '02/01/1212',
+      to: {'Giuliu': 1000, 'Cats': 2000, 'Berlino': 1000}),
+  Payment(
+      from: 'Berlino',
+      amount: 5,
+      category: 'Porno',
+      data: '03/01/1212',
+      to: {'Berlino': 2, 'Cats': 3}),
 ];
 
 Map<String, IconData> categoriesToIcon = {
-  'Discoteca': Icons.music_note,
-  'Porno': Icons.accessible_outlined,
-  'Spesa': Icons.shopping_cart,
-  'Telefono': Icons.phone,
-  'Trasporto': Icons.airplanemode_active,
-  'Casa': Icons.home,
-  'Ristorante': Icons.restaurant,
-  'Regalo': Icons.card_giftcard,
-  'Pagamento': Icons.payment
+  // 'Discoteca': Icons.music_note,
+  // 'Porno': Icons.accessible_outlined,
+  // 'Spesa': Icons.shopping_cart,
+  // 'Telefono': Icons.phone,
+  // 'Trasporto': Icons.airplanemode_active,
+  // 'Casa': Icons.home,
+  // 'Ristorante': Icons.restaurant,
+  // 'Regalo': Icons.card_giftcard,
+  // 'Pagamento': Icons.payment
 };
--- lib/screens/add_expense_screen.dart
+++ lib/screens/add_expense_screen.dart
@@ -11,9 +11,9 @@ class AddExpenseScreen extends StatefulWidget {
 }
 
 class _AddExpenseScreenState extends State<AddExpenseScreen> {
-  String choosedFrom = esempioCards[0].name;
+  String choosedFrom = '';
   int amount = 0;
-  String category = 'Discoteca';
+  String category = '';
 
   @override
   Widget build(BuildContext context) {
@@ -29,18 +29,20 @@ class _AddExpenseScreenState extends State<AddExpenseScreen> {
           ),
         ),
         leading: IconButton(
-          onPressed: () {
-            Scaffold.of(context).openDrawer();
-          },
           icon: const Icon(
             Icons.chevron_left,
             size: 36,
           ),
           color: Colors.grey[800],
+          onPressed: () {
+            Navigator.pop(context);
+          },
         ),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: () {
+              //TODO: aggiungi azione
+            },
             icon: const Icon(
               Icons.check,
               size: 28,
@@ -56,90 +58,155 @@ class _AddExpenseScreenState extends State<AddExpenseScreen> {
           child: Form(
             child: Column(
               children: [
-                DropdownButtonFormField(
-                  items: esempioCards.map((PersonalCard card) {
-                    return DropdownMenuItem(
-                      value: card.name,
-                      child: Text(card.name),
+                const SizedBox(height: 40.0),
+                FormField(
+                  builder: (FormFieldState state) {
+                    return InputDecorator(
+                      decoration: InputDecoration(
+                        labelText: 'Seleziona il nome',
+                        labelStyle: TextStyle(
+                          color: Colors.grey[400],
+                          fontFamily: 'Oxygen-Regular',
+                          fontSize: 20.0,
+                        ),
+                        icon:
+                            const Icon(Icons.person, color: Color(0xFFE57373)),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey[400]!),
+                          gapPadding: 10.0,
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                      ),
+                      isEmpty: choosedFrom == '',
+                      child: DropdownButtonHideUnderline(
+                        child: DropdownButton(
+                          isExpanded: true,
+                          isDense: true,
+                          dropdownColor: Colors.grey[600], //colore sfondo popup
+                          iconSize: 30.0,
+                          iconEnabledColor: Color(0xFFE57373),
+                          enableFeedback: true,
+                          elevation: 10,
+                          itemHeight: 70.0,
+                          items: esempioCards.map((PersonalCard card) {
+                            return DropdownMenuItem(
+                              value: card.name,
+                              alignment: Alignment.centerLeft,
+                              enabled: true,
+                              child: Text(
+                                card.name,
+                                style: TextStyle(
+                                  color: Colors.grey[200],
+                                  fontFamily: 'Oxygen-Regular',
+                                  fontSize: 23.0,
+                                ),
+                              ),
+                            );
+                          }).toList(),
+                          onChanged: (String? value) {
+                            setState(() {
+                              choosedFrom = value!;
+                            });
+                          },
+                          value: choosedFrom,
+                        ),
+                      ),
                     );
-                  }).toList(),
-                  onChanged: (String? value) {
-                    choosedFrom = value!;
-                  },
-                  value: choosedFrom,
-                  decoration: InputDecoration(
-                    labelText: 'Seleziona il nome',
-                    labelStyle: TextStyle(
-                      color: Colors.grey[400],
-                    ),
-                    enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Colors.grey[400]!,
-                      ),
-                    ),
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Colors.grey[400]!,
-                      ),
-                    ),
-                  ),
-                ),
-                TextFormField(
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                    labelText: "Inserisci l'importo in euro",
-                    labelStyle: TextStyle(
-                      color: Colors.grey[400],
-                    ),
-                    enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Colors.grey[400]!,
-                      ),
-                    ),
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Colors.grey[400]!,
-                      ),
-                    ),
-                  ),
-                  onSaved: (String? value) {
-                    String valueWithoutDot = value!.replaceAll('.', '');
-                    amount = int.parse(valueWithoutDot);
-                  },
-                  validator: (String? value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Inserisci un importo';
-                    }
-                    return null;
                   },
                 ),
-                DropdownButtonFormField(
-                    items: categoriesToIcon.keys.map((String category) {
-                      return DropdownMenuItem(
-                        value: category,
-                        child: Text(category),
-                      );
-                    }).toList(),
-                    onChanged: (String? value) {
-                      category = value!;
-                    },
-                    value: category,
-                    decoration: InputDecoration(
-                      labelText: 'Seleziona la categoria',
-                      labelStyle: TextStyle(
-                        color: Colors.grey[400],
-                      ),
-                      enabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Colors.grey[400]!,
+                const SizedBox(height: 40.0),
+                FormField(
+                  builder: (FormFieldState state) {
+                    return InputDecorator(
+                      decoration: InputDecoration(
+                        labelText: "Seleziona l'importo",
+                        labelStyle: TextStyle(
+                          color: Colors.grey[400],
+                          fontFamily: 'Oxygen-Regular',
+                          fontSize: 20.0,
+                        ),
+                        icon: const Icon(Icons.euro, color: Color(0xFFE57373)),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey[400]!),
+                          gapPadding: 10.0,
+                          borderRadius: BorderRadius.circular(10.0),
                         ),
                       ),
-                      focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Colors.grey[400]!,
+                      isEmpty: amount == 0,
+                      child: TextFormField(
+                        keyboardType: TextInputType.number,
+                        onSaved: (String? value) {
+                          String valueWithoutDot = value!.replaceAll('.', '');
+                          valueWithoutDot = valueWithoutDot.replaceAll(',', '');
+                          amount = int.parse(valueWithoutDot);
+                        },
+                        validator: (String? value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Inserisci un importo';
+                          }
+                          return null;
+                        },
+                      ),
+                    );
+                  },
+                ),
+                const SizedBox(height: 40.0),
+                FormField(
+                  builder: (FormFieldState state) {
+                    return InputDecorator(
+                      decoration: InputDecoration(
+                        labelText: 'Seleziona la categoria',
+                        labelStyle: TextStyle(
+                          color: Colors.grey[400],
+                          fontFamily: 'Oxygen-Regular',
+                          fontSize: 20.0,
+                        ),
+                        icon: const Icon(Icons.category,
+                            color: Color(0xFFE57373)),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey[400]!),
+                          gapPadding: 10.0,
+                          borderRadius: BorderRadius.circular(10.0),
                         ),
                       ),
-                    )),
+                      isEmpty: category == '',
+                      child: DropdownButtonHideUnderline(
+                        child: DropdownButton(
+                          isExpanded: true,
+                          isDense: true,
+                          dropdownColor: Colors.grey[600], //colore sfondo popup
+                          iconSize: 30.0,
+                          iconEnabledColor: Color(0xFFE57373),
+                          enableFeedback: true,
+                          elevation: 10,
+                          itemHeight: 70.0,
+                          items:
+                              categoriesToIcon.keys.map((String categoryName) {
+                            return DropdownMenuItem(
+                              value: categoryName,
+                              alignment: Alignment.centerLeft,
+                              enabled: true,
+                              child: Text(
+                                categoryName,
+                                style: TextStyle(
+                                  color: Colors.grey[200],
+                                  fontFamily: 'Oxygen-Regular',
+                                  fontSize: 23.0,
+                                ),
+                              ),
+                            );
+                          }).toList(),
+                          onChanged: (String? value) {
+                            setState(() {
+                              category = value!;
+                            });
+                          },
+                          value: category,
+                        ),
+                      ),
+                    );
+                  },
+                ),
               ],
             ),
           ),
